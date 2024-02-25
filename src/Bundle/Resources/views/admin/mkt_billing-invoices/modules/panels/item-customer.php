8a15e86d125ff49c81d28417cb92d8616ce993b5
Gabriel Solomon <hello@gabrielsolomon.ro>
2024-02-25 19:36:56 +0200

    fix: allow for invoices with no customer profile

--- src/Bundle/Resources/views/admin/mkt_billing-invoices/modules/panels/item-customer.php
+++ src/Bundle/Resources/views/admin/mkt_billing-invoices/modules/panels/item-customer.php
@@ -6,17 +6,20 @@ use ByTIC\AdminBase\Widgets\Cards\Card;
 use ByTIC\Icons\Icons;
 use Marktic\Billing\Utility\BillingModels;
 
+
 $card = Card::make()
     ->withTitle(BillingModels::parties()->getLabel('title.singular'))
     ->withIcon(Icons::list_ul())
     ->wrapBody(false)
     ->withView($this)
-    ->addHeaderTool(
+    ->withViewContent('/mkt_billing-parties/modules/item/details', ['item' => $this->customerParty]);
+if ($this->customerPart) {
+    $card->addHeaderTool(
         ButtonAction::make()
             ->setUrl($this->customerParty->compileURL('edit'))
             ->addHtmlClass('btn-xs')
             ->setLabel('Edit')
-    )
-    ->withViewContent('/mkt_billing-parties/modules/item/details', ['item' => $this->customerParty]);
+    );
+}
 ?>
 <?= $card; ?>
